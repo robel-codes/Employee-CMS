2a07e64089cc3278bfee6b72f8811cee305f4283
Robel <rofikre@yahoo.com>
2022-02-17 02:30:18 -0500

    created connection to database

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+//create connection to our database
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+
+module.exports = db;
