46268f06e90ee27c9cf02f455fb96b436e237f78
Robel <rofikre@yahoo.com>
2022-02-19 04:21:30 -0500

    corrected password and user link to the Database

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PW,
     database: process.env.DB_NAME
 });
 
